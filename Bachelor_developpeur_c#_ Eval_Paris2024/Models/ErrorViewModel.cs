bdcc29b9c0ec4b9843d30555db5da9c7024af35b
CorinDesbois <corinne.wilwert@yahoo.fr>
2025-07-18 15:49:27 +0200

    Add project files.

---
+++ Bachelor_developpeur_c#_ Eval_Paris2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bachelor_developpeur_c___Eval_Paris2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
