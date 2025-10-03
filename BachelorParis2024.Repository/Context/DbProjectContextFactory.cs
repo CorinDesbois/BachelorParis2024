66b914d19870c629063ffe2a3437fdacd2689c61
CorinDesbois <corinne.wilwert@yahoo.fr>
2025-10-03 18:11:57 +0200

    Modification de la connection au server . ( authentification intégrée Microsoft Entra)

--- BachelorParis2024.Repository/Context/DbProjectContextFactory.cs
+++ BachelorParis2024.Repository/Context/DbProjectContextFactory.cs
@@ -18,7 +18,7 @@ namespace BachelorParis2024.Repository.Context
         public DbProjectContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<DbProjectContext>();
-            optionsBuilder.UseSqlServer("Data Source=tcp:bachelorparis2024freeserver.database.windows.net,1433;Initial Catalog=BachelorParis2024FreeDB;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication= 'Active Directory Default'; Encrypt=True;");
+            optionsBuilder.UseSqlServer("Data Source=tcp:bachelorparis2024freeserver.database.windows.net,1433;Initial Catalog=BachelorParis2024FreeDB;Persist Security Info=False;User ID=corindesbois_live.fr#EXT#@corindesboislive.onmicrosoft.com;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication= 'Active Directory Integrated';");
             return new DbProjectContext(optionsBuilder.Options);
         }
     }
